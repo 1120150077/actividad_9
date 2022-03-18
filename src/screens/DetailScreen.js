6c4d5f896753e610106dcdfbe8c32cf4aa67f0eb
Ricardo <a1120150077@utch.edu.mx>
2022-03-18 13:12:17 -0700

    Version 1

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react' ;
+import { View, Text, Button, SafeAreaView } from "react-native";
+
+export default function SettingsScreen({ route, navigation }) {
+   const { itemId } = route.params;
+   const [post, setPost] = useState([]);
+
+   const getPost = async() => {
+
+    try{
+
+      const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;
+      //consumir datos
+      const response = await fetch(url); 
+      //convertir los json
+      const json = await response.json(); 
+
+      setPost(json);
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
+  }
+  useEffect(()=>{
+    getPost();
+  },[post])
+  return (
+    <SafeAreaView>
+      <Text>Id: {JSON.stringify(itemId)}</Text>
+      <Text>Titulo: {post.title}</Text>
+      <Text>Detalle: {post.body}</Text>
+      <Button onPress={() =>{
+        navigation.navigate('Home');
+      }} title="REGRESAR" />
+    </SafeAreaView>
+  );
+}
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, SafeAreaView, FlatList } from "react-native";
+
+export default function HomeScreen({navigation }) {
+
+  const [datos, setDatos] = useState([]);
+
+   const getPosts = async() => {
+    try{
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      //consumir datos
+      const response = await fetch(url); 
+      //convertir los json
+      const json = await response.json(); 
+      setDatos(json);
+    } catch (error) {
+      console.error(error);
+    } 
+  }
+
+
+  useEffect(()=>{
+    getPosts();
+  }, [])
+
+  return (
+    <SafeAreaView>
+      
+      <FlatList 
+              data={datos}
+              keyExtractor = { ({ id }, index) => id }
+              renderItem = {
+                ({item}) => (
+                  <Text onPress={
+
+                    () => {
+                      navigation.navigate('Detail',{ itemId: item.id })
+                    }
+
+                  }>{item.title}</Text>
+                )
+              }
+      />
+
+      <Button onPress={()=>{
+         navigation.navigate('Detail', {
+            itemId: 8,
+          });
+
+      }} title="Mostrar detalle del post 8" />
+
+
+    </SafeAreaView>
+  );
+}
